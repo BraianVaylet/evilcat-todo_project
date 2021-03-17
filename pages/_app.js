f79b66655a3cdf8a9b71b3b1bea1cf69d3f01eaa
braian vaylet <braianvaylet@eycon.com>
2021-03-17 18:09:50 -0300

    add manifest.json

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,11 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>EvilCat ToDo</title>
+        <title>EvilCat</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <Wrapper>
         <Component {...pageProps} />
